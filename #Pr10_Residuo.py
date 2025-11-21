16d6602210582a0558d845b5e9b00a51602c2f14
EscobarRuizAlexannder01 <martellrios@icloud.com>
2025-11-21 10:54:46 -0600

    Delete #Pr10_Residuo.py

--- #Pr10_Residuo.py
+++
@@ -1,6 +0,0 @@
-#Pr10_Residuo
-#Autor: Alexannder Escobar Ruiz
-Divisr = int (input("Coloca el numero que quieres dividir"))
-Dividnd = int (input("Coloca el numero por el cual lo quieras divir"))
-Residuo = Divisr%Dividnd 
-print("El residuo de la siguiente division es",Residuo)
